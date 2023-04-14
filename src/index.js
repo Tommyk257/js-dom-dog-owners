769aa3851b31cbf46304ae0a7798e9a9a4b5d585
Isabelle <isabel.tartarelli@live.it>
2023-04-14 19:44:12 +0100

    added name and picture to card

--- src/index.js
+++ src/index.js
@@ -2,19 +2,28 @@ console.log(data);
 
 // WRITE YOUR CODE BELOW!
 
-
-const ul = document.querySelector(".dogs-list")
+const ul = document.querySelector(".dogs-list");
 
 data.forEach((element) => {
+  const liEl = document.createElement("li");
+  liEl.className = "dogs-list__button";
+  liEl.innerText = element.name;
+  // console.log(liEl)
+
+  // console.log(dogImg)
+  ul.append(liEl);
+});
+
+const mainS = document.querySelector(".main__dog-section");
+
+data.forEach((dogCards) => {
+  const h2 = document.querySelector("h2");
+    h2.innerText = dogCards.name;
     
-    const liEl = document.createElement("li");
-    liEl.className = "dogs-list__button"
-    liEl.innerText = element.name
-    // console.log(liEl)    
-    
-    
-    ul.append(liEl);
-})
+  const dogImg = document.createElement("img");
+  dogImg.src = dogCards.image;
 
 
-
+  mainS.append(dogImg);
+  h2.append(mainS);
+});
