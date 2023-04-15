608153d93d2d14700719e50fa16c873c55857d45
tommyk257 <tommyk257@gmail.com>
2023-04-15 14:36:04 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -17,13 +17,34 @@ data.forEach((element) => {
 const mainS = document.querySelector(".main__dog-section");
 
 data.forEach((dogCards) => {
-  const h2 = document.querySelector("h2");
-    h2.innerText = dogCards.name;
+    const h2 = document.createElement("h2");
+      h2.innerText = dogCards.name;
+      
+    const dogImg = document.createElement("img");
+    dogImg.src = dogCards.image[0];
+  
+    const afterimg = document.createElement("div")
+    afterimg.className = "main__dog-section__desc"
     
-  const dogImg = document.createElement("img");
-  dogImg.src = dogCards.image;
+    const bio = document.createElement("h3")
+    bio.innerText = "Bio"
+    
+    const dogInfo = document.createElement("p")
+    dogInfo.innerText = dogCards.bio
+    
 
-
+    
+    
+    
   mainS.append(dogImg);
-  h2.append(mainS);
-});
+  mainS.append(h2);
+  mainS.append(bio);
+  afterimg.append(bio)
+  afterimg.append(dogInfo)
+ 
+})
+
+
+// weve created the title with dogs name
+// we need to create img tags to display dog images
+// create a 
