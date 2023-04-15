5ccb9d8f6e36eef1a89d1c8ab1dd1e1a96d5b334
Isabelle <isabel.tartarelli@live.it>
2023-04-15 15:51:04 +0100

    Removing title

--- src/index.js
+++ src/index.js
@@ -8,43 +8,50 @@ data.forEach((element) => {
   const liEl = document.createElement("li");
   liEl.className = "dogs-list__button";
   liEl.innerText = element.name;
-  // console.log(liEl)
 
-  // console.log(dogImg)
   ul.append(liEl);
 });
 
 const mainS = document.querySelector(".main__dog-section");
 
 data.forEach((dogCards) => {
-    const h2 = document.createElement("h2");
-      h2.innerText = dogCards.name;
-      
-    const dogImg = document.createElement("img");
-    dogImg.src = dogCards.image[0];
-  
-    const afterimg = document.createElement("div")
-    afterimg.className = "main__dog-section__desc"
-    
-    const bio = document.createElement("h3")
-    bio.innerText = "Bio"
-    
-    const dogInfo = document.createElement("p")
-    dogInfo.innerText = dogCards.bio
-    
+  const h2 = document.createElement("h2");
+  h2.innerText = dogCards.name;
+  h2.className = "dogsNames";
 
-    
-    
-    
-  mainS.append(dogImg);
+  const dogImg = document.createElement("img");
+  dogImg.src = dogCards.image;
+
+  const afterimg = document.createElement("div");
+  afterimg.className = "main__dog-section__desc";
+
+  const bio = document.createElement("h3");
+  bio.innerText = "Bio";
+
+  const dogInfo = document.createElement("p");
+  dogInfo.innerText = dogCards.bio;
+
   mainS.append(h2);
-  mainS.append(bio);
-  afterimg.append(bio)
-  afterimg.append(dogInfo)
- 
-})
+  mainS.append(dogImg);
 
+  mainS.append(bio);
+  mainS.append(afterimg);
+  afterimg.append(bio);
+  afterimg.append(dogInfo);
+});
+
+function changeTitle() {
+  const dogCards = document.querySelector("h2");
+  dogCards.remove();
+}
+
+const dogButton = document.querySelector("ul");
+dogButton.addEventListener("click", changeTitle);
 
 // weve created the title with dogs name
 // we need to create img tags to display dog images
-// create a 
+// create a
+
+// we need a loop to make the 'buttons' clickable to display each dog card
+
+// we need to display one card at the time creating a funcion linked to the first loop
