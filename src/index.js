dac57c4bba2a6e22c98a72f80268c7dee56341bc
Isabelle <isabel.tartarelli@live.it>
2023-04-16 13:01:38 +0100

    finished core

--- src/index.js
+++ src/index.js
@@ -4,49 +4,51 @@ console.log(data);
 
 const ul = document.querySelector(".dogs-list");
 
+
 data.forEach((element) => {
   const liEl = document.createElement("li");
   liEl.className = "dogs-list__button";
   liEl.innerText = element.name;
+ ul.append(liEl);
 
-  ul.append(liEl);
+
+  liEl.addEventListener("click", function() {
+    const mainS = document.querySelector(".main");
+    mainS.innerText = "";
+
+    const section = document.createElement("section")
+    section.setAttribute("class", "main__dog-section");
+    mainS.append(section)
+ 
+   const h2 = document.createElement("h2");
+   h2.innerText = element.name;
+    section.append(h2)
+    
+     const dogImg = document.createElement("img");
+    dogImg.src = element.image;
+    section.append(dogImg)
+
+       const afterimg = document.createElement("div");
+    afterimg.className = "main__dog-section__desc";
+    section.append(afterimg)
+
+ const bio = document.createElement("h3");
+    bio.innerText = "Bio";
+    afterimg.append(bio)
+
+
+ const dogInfo = document.createElement("p");
+  dogInfo.innerText = element.bio;
+afterimg.append(dogInfo)
+  })
+
 });
 
-const mainS = document.querySelector(".main__dog-section");
+// Create a function to loop each card
+// in the loop get each card separately - index -
+// dispay each different card when button is clicked
 
-data.forEach((dogCards) => {
-  const h2 = document.createElement("h2");
-  h2.innerText = dogCards.name;
-  h2.className = "dogsNames";
 
-  const dogImg = document.createElement("img");
-  dogImg.src = dogCards.image;
-
-  const afterimg = document.createElement("div");
-  afterimg.className = "main__dog-section__desc";
-
-  const bio = document.createElement("h3");
-  bio.innerText = "Bio";
-
-  const dogInfo = document.createElement("p");
-  dogInfo.innerText = dogCards.bio;
-
-  mainS.append(h2);
-  mainS.append(dogImg);
-
-  mainS.append(bio);
-  mainS.append(afterimg);
-  afterimg.append(bio);
-  afterimg.append(dogInfo);
-});
-
-function changeTitle() {
-  const dogCards = document.querySelector("h2");
-  dogCards.remove();
-}
-
-const dogButton = document.querySelector("ul");
-dogButton.addEventListener("click", changeTitle);
 
 // weve created the title with dogs name
 // we need to create img tags to display dog images
